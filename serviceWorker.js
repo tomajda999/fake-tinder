df74d30cf6be70b3b9d2c1f2286179af172eb5b3
PRO <pro@n-side.com>
2024-01-27 15:57:59 +0100

    Version 1.1

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,7 +4,7 @@
 		1) cacheAll = true 		=> Cache all requests in urlsToCache list and all further requests
 		2) cacheAll = false 	=> Cache only all requests in urlsToCache list
 */
-const version = "1.0"
+const version = "1.1"
 var cacheAll = false;
 var CACHE_NAME = 'fake-tinder-cache';
 var urlsToCache = [
