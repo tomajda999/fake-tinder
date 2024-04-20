71a0724b53ec9a7c59fec0fa1c1611e1275920c7
tomajda999 <73846501+tomajda999@users.noreply.github.com>
2024-04-20 09:32:21 +0200

    Update tinder.js

--- js/tinder.js
+++ js/tinder.js
@@ -2,11 +2,11 @@
 // Tinder
 
 let imgCount = 0
-const appLink = 'pablorotten.github.io/fake-tinder'
+const appLink = 'tomajda999.github.io/fake-tinder'
 const profiles = [
-  {img: `images/profiles/calamardo.jpeg`, name: 'Squid', age: '45', distance: '6'},
-  {img: `images/profiles/amongus-border1.gif`, name: 'Suspisus', age: '19', distance: '12'},
-  {img: `images/profiles/gigachad.jpeg`, name: 'Chad', age: '30', distance: '13'},
+  {img: `images/profiles/Adam.jpg`, name: 'Squid', age: '45', distance: '6'},
+  {img: `images/profiles/Ada.jpg`, name: 'Suspisus', age: '19', distance: '12'},
+  {img: `images/profiles/Kacka.jpg`, name: 'Chad', age: '30', distance: '13'},
   {img: `images/profiles/alo.jpeg`, name: 'Fernanlove', age: '40', distance: '11'},
   {img: `images/profiles/m&m.png`, name: 'Miss M&M', age: '23', distance: '5'},
   {img: `images/profiles/broly.png`, name: 'Broly', age: '31', distance: '13'},
